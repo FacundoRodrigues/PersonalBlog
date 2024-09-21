91de9d40cee55e0ce90f50d642b4b730c239cab2
Facundo Rodrigues <facuurodrigues@gmail.com>
2024-09-21 20:14:05 -0300

    feat: init linter

--- personal-blog/eslint.config.js
+++ personal-blog/eslint.config.js
@@ -1,38 +1,29 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import react from 'eslint-plugin-react'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+
+// Importar el plugin para la eliminación de variables no usadas
+import eslintPluginUnusedImports from "eslint-plugin-unused-imports";
 
 export default [
-  { ignores: ['dist'] },
   {
-    files: ['**/*.{js,jsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-      parserOptions: {
-        ecmaVersion: 'latest',
-        ecmaFeatures: { jsx: true },
-        sourceType: 'module',
-      },
-    },
-    settings: { react: { version: '18.3' } },
-    plugins: {
-      react,
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-    },
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
     rules: {
-      ...js.configs.recommended.rules,
-      ...react.configs.recommended.rules,
-      ...react.configs['jsx-runtime'].rules,
-      ...reactHooks.configs.recommended.rules,
-      'react/jsx-no-target-blank': 'off',
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
+      // Esta regla activa advertencias para variables e imports no usados
+      "no-unused-vars": "warn",  
+      // Plugin que elimina automáticamente los imports no utilizados
+      "unused-imports/no-unused-imports": "error",  
     },
   },
-]
+  {
+    files: ["**/*.js"],
+    languageOptions: { sourceType: "script" },
+  },
+  {
+    languageOptions: { globals: globals.browser },
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+];
